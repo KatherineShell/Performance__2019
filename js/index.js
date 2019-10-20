dbcfab7aab8b9ced686b4c226628766d6fb05f1b
Katherine <katya-kima@mail.ru>
2019-10-20 22:32:40 +0300

    revert

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
    const buttonsContainer = document.querySelector('.buttons-wrap');
-console.log('buttons-wrap')
+console.log('buttons-wrap', buttonsContainer)
     buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
         '<button class="button">Нет</button>';
     
