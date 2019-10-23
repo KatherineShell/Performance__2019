73c191d01f57de5cc1b9ed86dc50222a3f59a0fb
Katherine <katya-kima@mail.ru>
2019-10-23 13:24:17 +0300

    comment inner html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const buttonsContainer = document.querySelector('.buttons-wrap');
-buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-    '<button class="button">Нет</button>';
+// buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
+//     '<button class="button">Нет</button>';
 const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
 const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
 const purchaseHtml = '<div class="purchase-list-wrap"><p class="card-description card-description_big description_critical">Список покупок:</p><ol class="purchase-list"><li class="purchase-list__item">Хлеб</li><li class="purchase-list__item">Молоко</li></ol></div>';
